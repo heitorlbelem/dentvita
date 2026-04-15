cd7ab7c87e5b92edf3177715da80f5440a252601
Heitor Belém <heitorlbelem98@gmail.com>
2026-04-15 14:39:32 -0300

    creates `database.js` module to connect to db

---
+++ infra/database.js
@@ -0,0 +1,44 @@
+import { Client } from "pg";
+
+async function query(queryString) {
+  let client;
+
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryString);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
+}
+
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLOptions(),
+  });
+
+  await client.connect();
+  return client;
+}
+
+function getSSLOptions() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "production" ? true : false;
+}
+
+export default {
+  query,
+  getNewClient,
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,25 @@
+import database from "../../../../infra/database";
+
 async function status(request, response) {
+  const databaseVersion = await database.query("SHOW SERVER_VERSION;");
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenConnections = await database.query({
+    text: "SELECT count(*)::int opened_connections from pg_stat_activity WHERE datname=$1;",
+    values: [databaseName],
+  });
+
   return response.status(200).json({
     updated_at: new Date().toISOString(),
+    dependencies: {
+      database: {
+        version: databaseVersion.rows[0].server_version,
+        max_connections: new Number(
+          databaseMaxConnections.rows[0].max_connections,
+        ),
+        opened_connections: databaseOpenConnections.rows[0].opened_connections,
+      },
+    },
   });
 }
 
