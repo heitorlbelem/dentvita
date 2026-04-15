b19d124dbcf8a66de306daa9231e46a8ab2aafdd
Heitor Belém <heitorlbelem98@gmail.com>
2026-04-15 14:39:09 -0300

    implements new tests scenarios for `api/v1/status` endpoint

--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -6,4 +6,13 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.updated_at).toBeDefined();
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(parsedUpdatedAt).toEqual(responseBody.updated_at);
+
+  const databaseResponseData = responseBody.dependencies.database;
+  expect(databaseResponseData).toBeDefined();
+  expect(databaseResponseData.version).toBeDefined();
+  expect(databaseResponseData.max_connections).toBeDefined();
+  expect(databaseResponseData.opened_connections).toBeDefined();
+  expect(databaseResponseData.version).toEqual("16.0");
+  expect(databaseResponseData.max_connections).toEqual(100);
+  expect(databaseResponseData.opened_connections).toEqual(1);
 });
